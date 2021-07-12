45de231ca2ad15ec421bca47e7390ef07b4b9cb9
Adam Kaszyński <Yugo1212@outlook.com>
2021-07-12 22:43:16 +0200

    Added bunch of new stuff, products, company list, registration and roles.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BulkyBookApp.DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BulkyBookApp.Areas.Identity.IdentityHostingStartup))]
+namespace BulkyBookApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
