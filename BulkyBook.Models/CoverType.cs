fa57f618e8f874a540cf355422679a7d825646ce
Adam Kaszyński <73426421+Yugo1212@users.noreply.github.com>
2021-08-08 19:14:19 +0200

    Add files via upload

---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBookApp.Models
+{
+    public class CoverType
+    {
+        public int Id { get; set; }
+        [Display(Name="Cover Type")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
